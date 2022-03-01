ff35ad587044ccce78112137c102dd539afab972
quantumomid <omid.wakili@outlook.com>
2022-03-01 16:07:01 +0000

    sub application execution context - taking care/account of different contexts which the Products sub app could be run in or through

--- ecommerce/products/src/bootstrap.js
+++ ecommerce/products/src/bootstrap.js
@@ -1,11 +1,27 @@
 import faker from "faker";
 
-let products = "";
+const mount = (el) => {
+    let products = "";
 
-for (let i=0; i<7; i++){
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`;
+    for (let i=0; i<7; i++){
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`;
+    }
+    
+    // console.log(products);
+    el.innerHTML = products;
+};
+
+// Context/Situation #1 - running file in development in isolation
+// First check to see in development
+if (process.env.NODE_ENV === "development") {
+
+    // Check to see we are indeed in isolation i.e. using local index.html file and NOT through Container app
+    const el = document.querySelector("#dev-products") // Because dev-products <div> only exists in the local html file (ASSUMPTION)
+    if(el) {
+        mount(el);
+    }
 }
 
-// console.log(products);
-document.querySelector('#dev-products').innerHTML = products;
+// Context/Situation #2 - when running through Container app
+export { mount };
