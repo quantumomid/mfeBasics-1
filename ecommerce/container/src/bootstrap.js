ba2130d7aba1c58ed5ef99fc7c4981bdfeab0ecb
quantumomid <omid.wakili@outlook.com>
2022-03-01 16:13:00 +0000

    refactor Container to work with context/situation 2 inside products and set the root file as bootstrap as that is where the mount function is exported from

--- ecommerce/container/src/bootstrap.js
+++ ecommerce/container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import "products/ProductsIndex";
+import { mount } from "products/ProductsIndex";
 import "cart/CartShow";
 
 console.log("Container 👾");
+
+const el = document.querySelector("#my-products");
+mount(el);
