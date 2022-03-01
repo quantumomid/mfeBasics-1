0634dd132a1d8c3abae4825679aabe6539e1bfa5
quantumomid <omid.wakili@outlook.com>
2022-03-01 20:52:51 +0000

    add comment to explain naming of div id in Container and also fix bug with same el being used for both mount methods

--- ecommerce/container/src/bootstrap.js
+++ ecommerce/container/src/bootstrap.js
@@ -3,6 +3,9 @@ import { mount as cartMount } from "cart/CartShow";
 
 console.log("Container 👾");
 
-const el = document.querySelector("#my-products");
-productsMount(el);
-cartMount(el);
+productsMount(document.querySelector("#my-products"));
+cartMount(document.querySelector("#my-cart"));
+
+// Note: although you are free to name above as you wish: ideally:
+// 1) Do not name same as the <div> id of the local index.html files in the Products and Cart subfolders
+// 2) Also do not give the same name as 'name' attribute used in the ModuleFederationPlugin in the webpack configs of the Products and Carts subfolders - as this creates a bizare bug that interferes with webpack in the background!
